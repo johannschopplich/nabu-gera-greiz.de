22a537f7fa7731f75e33e781886b4907384c4495
Johann Schopplich <mail@johannschopplich.com>
2021-08-30 11:19:03 +0200

    chore: remove tracked vendor folder

--- site/plugins/nabu-gera-greiz/index.js
+++ site/plugins/nabu-gera-greiz/index.js
@@ -1,8 +1,8 @@
 /* global panel */
-panel.plugin('nabu-gera-greiz/blocks', {
+panel.plugin("nabu-gera-greiz/blocks", {
   blocks: {
     line: `
       <hr :class="'is-' + content.class" />
-    `
-  }
-})
+    `,
+  },
+});
